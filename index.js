957701a5f1d56768249b1aa3bab6e6281896da63
Rafał Maciejewski <rafaljestsuper@gmail.com>
2020-04-03 22:15:57 +0200

    feat: inline includes in single templates

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+const includeRegex = /({% include '([\d.\/\-\w@_]*)'.*%})/im;
+
 module.exports = function loader(source) {
-  return source;
+  let output = `${source}`;
+  let match = output.match(includeRegex);
+
+  while ((match = output.match(includeRegex))) {
+    const includeString = match[1];
+    const templatePath = match[2];
+    const resolvedTemplatePath = path.join(this.context, templatePath);
+    this.addDependency(resolvedTemplatePath);
+    const template = fs.readFileSync(resolvedTemplatePath, 'utf8');
+    output = output.replace(includeString, template);
+  }
+
+  return output;
 };
